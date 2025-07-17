0d0ca5cb7b649473e084f4841965e389ddee3e09
abhijain <abhishek8875171881@gmail.com>
2025-07-17 15:56:50 +0530

    abhi

---
+++ GUI.py
@@ -0,0 +1,174 @@
+import io
+import streamlit as st
+import pandas as pd
+import datacleaning as dc
+import time
+import glob
+# pd.set_option("display.max_columns",None)
+# pd.set_option("display.width",1000)
+st.set_page_config(page_title="Dset Cheaning",page_icon="logo.png",layout="wide")
+st.markdown("""
+<style>
+.elbt1zu4
+{
+padding:0px 5px;
+}
+#dset-cleaning
+{
+padding:0px;
+margin-left:10px;
+margin-top:10px;
+color: rgb(239 106 7);
+font-size:250%;
+}
+#make-the-dataset-clean-in-one-click
+{
+text-align:center;
+}
+#stBaseButton-secondary
+{
+visibility:"hidden";
+}
+.e16xj5sw0
+{
+height:500px;
+margin:50px;
+margin-bottom:10px;
+display:flex;
+align-items:center;
+justify-content:center;
+margin-top: 20px;
+}
+.e16xj5sw1
+{
+margin-right:0;
+display:block;
+}
+.e16xj5sw2
+{
+    width: 100%;
+    margin-right: 1rem;
+    text-align: center;
+    display: block;
+    color:black;
+}
+.st-emotion-cache-bfgnao
+{
+    color: rgb(239 106 7);
+    margin:0 auto;
+    font-family: serif;
+    font-weight: 600;
+    font-size: 150%;
+    width:160px;
+}
+.e16xj5sw5
+{
+background-color: orange;
+margin-bottom: 20px;
+}
+.stToastContainer
+{
+display:flex;
+align-items:center;
+justify-content:center;
+}
+.stAlertContainer
+{
+text-align:center;
+}
+.st-emotion-cache-1gulkj5 .st-emotion-cache-1rwb540
+{
+position:absolute;
+visibility:hidden;
+}
+.stDownloadButton .st-emotion-cache-1rwb540
+{
+width:100vw;
+margin-bottom:20px;
+}
+.st-emotion-cache-1an99fx
+{
+    margin: 2% 3%;
+    background-color: gainsboro;
+    padding: 15px;
+    font-size: 120%;
+    font-weight: 600;
+    border-radius: 5px;
+    
+}
+.footer
+{
+    padding: 20px;
+    font-family: serif;
+    font-size: 120%;
+    background: antiquewhite;
+    margin-bottom: 10px;
+}
+</style>""",unsafe_allow_html=True)
+
+col1=st.columns(4)
+with col1[0]:
+  st.header("Dset-Cleaning")
+st.title("Make The Dataset Clean In One Click")
+st.info("The last column is assumed to represent the output feature for model training!")
+st.file_uploader(label="upload dataset file",type=["csv"],key="file")
+# print(st.session_state)
+df=None
+if st.session_state["file"] !=None:
+  st.toast("🎉 File uploaded successfully!", icon="📁")
+  ds_file=None
+  m=0
+  with st.spinner("Processing your file..."):
+    df = st.session_state['file']
+    dataset = pd.read_csv(df)
+    data1=dc.dataClaning(dataset)
+    file1=io.StringIO()
+    data1.to_csv(file1,index=False)
+    ds_file=file1.getvalue()
+    m=1
+
+    time.sleep(5)  # simulate some processing
+    st.success("✅ Done!")
+
+  if m==1 :
+    st.download_button(
+      label="📁 Download CSV",
+      data=ds_file,
+      file_name=st.session_state["file"].name.replace(".csv", "_Dset-Cleaning.csv"),
+      mime="text/csv"
+    )
+
+st.markdown("""
+<style>
+.st-emotion-cache-xhkv9f
+{
+  margin: 1% 4%;
+  border: 1px solid gray;
+  border-radius: 10px;
+  # box-shadow: 10px 10px 10px gray;
+  padding: 8px;
+  background-color: rgb(255 127 21);
+  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+}
+</style>""",unsafe_allow_html=True)
+st.image("ds_image.png",use_container_width=True)
+content="""🧼 Dset Cheaning — The Ultimate Dataset Cleanup Companion Say goodbye to messy data and hello to smooth preprocessing. Dset Cheaning is a robust, intuitive platform crafted for data analysts, ML engineers, and curious coders who need clean, reliable datasets without the usual hassle.
+
+🔍 Whether you're tackling thousands of rows or just a small sample, Dset Cheaning helps you:
+
+🚫 Eliminate duplicate entries for lean, meaningful data
+
+📉 Detect and remove outliers to preserve statistical integrity
+
+⚠️ Handle missing values with intelligent imputation and minimal data loss
+
+🎯 Ensure consistency across your dataset for streamlined analysis
+
+With powerful tools and a clean interface, Dset Cheaning makes data preparation feel less like a chore and more like progress. Your insights deserve clean input—let us help you get there."""
+st.text(content)
+
+st.markdown("""<div class="footer">© 2025 Dset Cheaning. All rights reserved.</div>""",unsafe_allow_html=True)
+
+
+
+
---
+++ datacleaning.py
@@ -0,0 +1,103 @@
+import pandas as pd
+import numpy as np
+
+def isnullPresent(dataset):
+    return dataset.isnull().sum().sum() != 0
+
+def findNullColumns(dataset):
+    return dataset.columns[dataset.isnull().any()].tolist()
+
+def nullDataSize(dataset):
+    v1 = dataset.shape[0] * dataset.shape[1]
+    v2 = dataset.dropna().shape[0] * dataset.dropna().shape[1]
+    return ((v2 / v1) * 100)
+
+def outputColumnTest(lst, dataset):
+    lst_copy = lst.copy()
+    for cl in lst_copy:
+        if cl == dataset.columns[-1]:
+            lst.remove(cl)
+            if ((dataset[cl].dtype != np.int64) and (len(dataset[cl].unique()) <= 10)) or \
+               ((dataset[cl].dtype == np.int64) and (len(dataset[cl].unique()) <= 2)):
+                return [True, 1, cl]
+            else:
+                return [True, 0, cl]
+    target = dataset.columns[-1]
+    if ((dataset[target].dtype != np.int64) and (len(dataset[target].unique()) <= 10)) or \
+       ((dataset[target].dtype == np.int64) and (len(dataset[target].unique()) <= 2)):
+        return [False, 1, target]
+    else:
+        return [False, 0, target]
+
+def outputColumnTreating(lst, dataset):
+    if lst[0]:
+        v2 = dataset[lst[2]].shape[0]
+        v1 = dataset[lst[2]].isnull().sum()
+        if (v1 / v2) * 100 <= 8:
+            dataset.dropna(subset=[lst[2]], inplace=True)
+        else:
+            if dataset[lst[2]].dtype == np.int64:
+                dataset.fillna({lst[2]: dataset[lst[2]].mean()}, inplace=True)
+            else:
+                dataset.fillna({lst[2]: dataset[lst[2]].mode()[0]}, inplace=True)
+
+def treatColumns(dataset, size, lst):
+    for l in lst:
+        v1 = dataset[l].isnull().sum()
+        v2 = dataset[l].shape[0]
+        if ((v1 / v2) * 100 >= 70):
+            dataset.drop(columns=[l], inplace=True)
+        else:
+            retained = dataset.dropna(subset=[l])
+            per = (retained.shape[0] * retained.shape[1]) / size * 100
+            if per >= 90:
+                dataset.dropna(subset=[l], inplace=True)
+            else:
+                if np.issubdtype(dataset[l].dtype, np.number):
+                    dataset.fillna({l: dataset[l].mean()}, inplace=True)
+                else:
+                    dataset.fillna({l: dataset[l].mode()[0]}, inplace=True)
+
+def treatOutlairs(dataset):
+    lst = dataset.columns
+    for l in lst:
+        if np.issubdtype(dataset[l].dtype, np.number):
+            skewness = dataset[l].skew()
+            if -1 <= skewness <= 1:
+                pass
+            else:
+                q1 = dataset[l].quantile(.25)
+                q3 = dataset[l].quantile(.75)
+                iqr = q3 - q1
+                if skewness < 0:
+                    data1 = dataset[dataset[l] >= (q3 - (1.5 * iqr))]
+                    min = dataset[l].mean() - (3 * dataset[l].std())
+                    data2 = dataset[dataset[l] >= min]
+                else:
+                    data1 = dataset[dataset[l] <= (q3 + (1.5 * iqr))]
+                    min = dataset[l].mean() + (3 * dataset[l].std())
+                    data2 = dataset[dataset[l] <= min]
+                dataset = data1 if data1.shape[0] >= data2.shape[0] else data2
+
+def dataClaning(dataset):
+    if isnullPresent(dataset):
+        per_null = nullDataSize(dataset)
+        if per_null <= 8:
+            data=dataset.dropna()
+            treatOutlairs(data)
+            data.drop_duplicates(inplace=True)
+            return data
+        else:
+            size = dataset.shape[0] * dataset.shape[1]
+            null_column_lst = findNullColumns(dataset)
+            ocl_type = outputColumnTest(lst=null_column_lst, dataset=dataset)
+            outputColumnTreating(ocl_type, dataset)
+            treatColumns(dataset=dataset, size=size, lst=null_column_lst)
+            treatOutlairs(dataset)
+    else:
+      treatOutlairs(dataset)
+    dataset.drop_duplicates(inplace=True)
+    return dataset
+
+if __name__ == "__main__":
+    print("main file")
